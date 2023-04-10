8583b57f0908728a314291513fc4330d0adabb18
Bilel GATRI <bilelgatri@gmail.com>
2023-04-10 19:32:44 +0200

    button to send message is successfully reached, automatic message sent back  to ask more details after each received message from gpt

--- content.js
+++ content.js
@@ -3,10 +3,9 @@ let previousMessage = '';
 let previousMessageTimestamp = 0;
 
 function checkForNewCommands() {
-	
-  const newMessage = document.querySelector('div.group:nth-last-child(2) div.markdown.prose').innerText;
+  const newMessage = document.querySelector('div.group:nth-last-child(2) div.markdown.prose').innerText.trim();
 
-// Continue reading the most  recent received message until it doesnt change during 4 seconds
+  // Continue reading the most recent received message until it doesn't change during 4 seconds
   if (newMessage !== previousMessage) {
     previousMessage = newMessage;
     previousMessageTimestamp = Date.now();
@@ -14,20 +13,81 @@ function checkForNewCommands() {
     if (newMessage !== lastCommand) {
       lastCommand = newMessage;
       console.log('Most recent received message:', lastCommand);
-	  
-     // Replace the following line with the appropriate code to find the chat input element
-    // const chatInputElement = document.querySelector('#chatInput');
-    // chatInputElement.value = command;
-    // chatInputElement.dispatchEvent(new Event('submit'));
-	
-					//or 
-					
-	//Process the command and send the output back
-	// Send the command to the background script
-    //chrome.runtime.sendMessage({ command });
+      sendFeedBack('needs more details');
     }
   }
 }
+
+function sendFeedBack(message) {
+  const textarea = document.querySelector('textarea[placeholder="Send a message..."]');
+  const sendButton = document.querySelector('textarea[placeholder="Send a message..."]').parentElement.parentElement.querySelector('button');
+
+  textarea.value = message;
+
+  // Trigger the input event to let the website know the textarea value has changed
+  const inputEvent = new Event('input', { bubbles: true });
+  textarea.dispatchEvent(inputEvent);
+
+  // Check if the send button is active and click it
+  if (!sendButton.hasAttribute('disabled')) {
+    sendButton.click();
+  } else {
+    console.log("Send button is not active.");
+  }
+}
+
+
+// function sendFeedBack(message) {
+  // const textarea = document.querySelector('textarea[placeholder="Send a message..."]');
+  // const sendButton = document.querySelector('textarea[placeholder="Send a message..."]').parentElement.parentElement.querySelector('button');;
+
+  // textarea.value = message;
+
+  // // Trigger the input event to let the website know the textarea value has changed
+  // const inputEvent = new Event('input', { bubbles: true });
+  // textarea.dispatchEvent(inputEvent);
+
+  // // Wait until the send button is active and click it
+  // const checkSendButtonActive = setInterval(() => {
+    // const activeSendButton = document.querySelector('button:not([disabled]):not([style*="display"])');
+    // if (activeSendButton) {
+      // activeSendButton.click();
+      // clearInterval(checkSendButtonActive);
+    // }
+  // }, 100);
+// }
+
+
+// function sendFeedBack(message) {
+  // const textarea = document.querySelector('textarea[placeholder="Send a message..."]');
+  // const sendButton = document.querySelector('button[disabled]:not([style*="display"])');
+
+  // textarea.value = message;
+
+  // // Trigger the input event to let the website know the textarea value has changed
+  // const inputEvent = new Event('input', { bubbles: true });
+  // textarea.dispatchEvent(inputEvent);
+
+  // // Observe the send button and click it when it becomes enabled
+   // observeSendButton(sendButton, () => {
+    // sendButton.click();
+  // });
+// }
+
+// function observeSendButton(button, callback) {
+  // const observer = new MutationObserver((mutations) => {
+    // mutations.forEach((mutation) => {
+      // if (mutation.type === 'attributes' && mutation.attributeName === 'disabled') {
+        // if (!button.hasAttribute('disabled')) {
+          // observer.disconnect();
+          // callback();
+        // }
+      // }
+    // });
+  // });
+
+  // observer.observe(button, { attributes: true });
+// }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.start) {
@@ -35,6 +95,3 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     sendResponse({ message: 'Started monitoring chat' });
   }
 });
-
-
-
--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const startButton = document.querySelector('#startButton');
+  const stopButton = document.querySelector('#stopButton');
   const logElement = document.querySelector('#log');
 
   function logMessage(message) {
@@ -10,6 +11,19 @@ document.addEventListener('DOMContentLoaded', () => {
     logMessage('Start button clicked');
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, { start: true }, (response) => {
+        if (chrome.runtime.lastError) {
+          logMessage('Error: ' + chrome.runtime.lastError.message);
+        } else {
+          logMessage(response.message);
+        }
+      });
+    });
+  });
+
+  stopButton.addEventListener('click', () => {
+    logMessage('Stop button clicked');
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { stop: true }, (response) => {
         if (chrome.runtime.lastError) {
           logMessage('Error: ' + chrome.runtime.lastError.message);
         } else {
