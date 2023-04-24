266670b4dd4efbed17a6d42bc161829077a824c1
Bilel GATRI <bilelgatri@gmail.com>
2023-04-24 16:06:19 +0200

    More stylish IHM whith one button

--- popup.js
+++ popup.js
@@ -24,37 +24,40 @@
 // # limitations under the License.
 
 document.addEventListener('DOMContentLoaded', () => {
-  const startButton = document.querySelector('#startButton');
-  const stopButton = document.querySelector('#stopButton');
+  const startStopButton = document.querySelector('#startButton');
   const logElement = document.querySelector('#log');
+  let monitoring = false;
 
   function logMessage(message) {
     logElement.textContent += message + '\n';
   }
 
-  startButton.addEventListener('click', () => {
-    logMessage('Start button clicked');
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { start: true }, (response) => {
-        if (chrome.runtime.lastError) {
-          logMessage('Error: ' + chrome.runtime.lastError.message);
-        } else {
-          logMessage(response.message);
-        }
+  startStopButton.addEventListener('click', () => {
+    monitoring = !monitoring;
+    if (monitoring) {
+      startStopButton.innerHTML = '<i class="fas fa-stop"></i> Stop';
+      logMessage('Start button clicked');
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { start: true }, (response) => {
+          if (chrome.runtime.lastError) {
+            logMessage('Error: ' + chrome.runtime.lastError.message);
+          } else {
+            logMessage(response.message);
+          }
+        });
       });
-    });
-  });
-
-  stopButton.addEventListener('click', () => {
-    logMessage('Stop button clicked');
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { stop: true }, (response) => {
-        if (chrome.runtime.lastError) {
-          logMessage('Error: ' + chrome.runtime.lastError.message);
-        } else {
-          logMessage(response.message);
-        }
+    } else {
+      startStopButton.innerHTML = '<i class="fas fa-play"></i> Start';
+      logMessage('Stop button clicked');
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { stop: true }, (response) => {
+          if (chrome.runtime.lastError) {
+            logMessage('Error: ' + chrome.runtime.lastError.message);
+          } else {
+            logMessage(response.message);
+          }
+        });
       });
-    });
+    }
   });
 });
